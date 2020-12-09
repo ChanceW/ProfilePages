5b6cca0c4ffcc7067f6ec59b1cf09870e26cbc3f
chancew <chancewilliams171@gmail.com>
2020-12-09 09:10:19 -0500

    Separating Profile List

--- NuCampProfiles/ClientApp/src/App.js
+++ NuCampProfiles/ClientApp/src/App.js
@@ -1,29 +1,9 @@
 import logo from './nl.PNG';
-import trophy from './trophy.svg';
 import './App.css';
 import React, { useState } from 'react';
-import { Card, CardBody, CardTitle, Button, Input } from 'reactstrap';
+import { Input } from 'reactstrap';
 import { AppConfig } from './AppConfig';
-import { Snapshots } from './Snapshots';
-import Confetti from 'react-confetti'
-
-function getCards(selectedClass) {
-    const classProfiles = AppConfig.classes.filter(c => { return c.name === selectedClass })[0].profiles;
-    return classProfiles.map((p, idx) => {
-        const url = p.external ? p.index : `/profiles/${p.index}`;
-        return <Card key={idx}>
-            <CardBody>
-                {p.winner ? <Confetti height="500" width="500" /> : null}
-                <CardTitle tag="h5"><a className="App-link" rel="noreferrer" href={url} target="_blank">{p.Name}</a></CardTitle>
-                <img className="snapshot" src={Snapshots[p.id]} />
-                {p.winner ? <img className="trophy" src={trophy} /> : null}
-                <br />
-                <Button rel="noreferrer" href={url} target="_blank">Visit</Button>
-            </CardBody>
-        </Card>
-    });
-}
-
+import ProfileList from './components/ProfileList';
 
 function App() {
     const classes = AppConfig.classes.map((c, idx) => { return <option key={idx}>{c.name}</option> });
@@ -37,7 +17,7 @@ function App() {
             <div className="App-Body">
                 <Input className="classSelect" type="select" onChange={(e) => { setSelected(e.target.value) }}>{classes}</Input>
                 <div className="cardList row">
-                    {getCards(selectedClass)}
+                    <ProfileList selectedClass={selectedClass} />
                 </div>
             </div>
         </div>
---
+++ NuCampProfiles/ClientApp/src/components/ProfileList.js
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+import { Card, CardBody, CardTitle, Button } from 'reactstrap';
+import { AppConfig } from '../AppConfig';
+import { Snapshots } from '../Snapshots';
+import Confetti from 'react-confetti';
+import trophy from '../trophy.svg';
+
+function ProfileList({ selectedClass }) {
+    const classProfiles = AppConfig.classes.filter(c => { return c.name === selectedClass })[0].profiles;
+    return classProfiles.map((p, idx) => {
+        const url = p.external ? p.index : `/profiles/${p.index}`;
+        return <Card key={idx}>
+            <CardBody>
+                {p.winner ? <Confetti height="500" width="500" /> : null}
+                <CardTitle tag="h5"><a className="App-link" rel="noreferrer" href={url} target="_blank">{p.Name}</a></CardTitle>
+                <img className="snapshot" src={Snapshots[p.id]} />
+                {p.winner ? <img className="trophy" src={trophy} /> : null}
+                <br />
+                <Button rel="noreferrer" href={url} target="_blank">Visit</Button>
+            </CardBody>
+        </Card>
+    });
+}
+
+export default ProfileList;

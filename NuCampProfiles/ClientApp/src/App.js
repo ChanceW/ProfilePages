8ed60b97c96f66b6ac3ebf3f03c65f84f91b45f9
chancew <chancewilliams171@gmail.com>
2021-02-18 09:46:30 -0500

    Sytling

--- NuCampProfiles/ClientApp/src/App.js
+++ NuCampProfiles/ClientApp/src/App.js
@@ -11,10 +11,10 @@ function App() {
     const comment = AppConfig.classes.filter(c => { return c.name === selectedClass })[0].comment;
     return (
         <div className="App">
-            <header className="App-header pt-2 f-5">
+            <header className="App-header p-4 f-5">
                 <div className="container">
                     <div className="media">
-                        <img className="mr-3 float-left float-md-none App-logo" src={logo} alt="Generic placeholder image" />
+                        <img className="my-auto mr-2 float-left float-md-none App-logo" src={logo} alt="Generic placeholder image" />
                         <div className="media-body align-self-center">
                             <h3 className="mt-0">Congratulations on Your BootCamp Progress, NuCamp Students!</h3>
                             <h5>Time to Showcase Your Skills</h5>
